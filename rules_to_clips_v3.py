9f6a1c5aaf1195ecb8ee5e74262ff593080c18c3
juanvs00 <55741983+juanvs00@users.noreply.github.com>
2022-09-03 13:17:51 +0200

    Add files via upload

--- rules_to_clips_v3.py
+++ rules_to_clips_v3.py
@@ -36,7 +36,7 @@ def reset_environment(env):
             elif str(fact).startswith("(available_object"):
                 fact.retract()
                 no_change = False
-            elif str(fact).startswith("(caution_loop"):
+            elif str(fact).startswith("(caution_loop)"):
                 fact.retract()
                 no_change = False
     # Resetear el hecho PENDING FUNCTION CALLS
@@ -73,66 +73,24 @@ def reset_position(env, memory):
     memory.clear()
 
 
-# Actualizar la posición y la orientación del agente dependiendo de sus movimientos
-def refresh_position(env, j):
-    pos, ori = where_I_am(env)
-
-    if j == "self.move_forward()":
-        x = int(pos[0]) + int(ori[0])
-        y = int(pos[1]) + int(ori[1])
-        for fact in env.facts():
-            if str(fact).startswith("(pos "):
-                fact.retract()
-                position = "(pos " + str(x) + " " + str(y) + ")"
-                env.assert_string(position)
-
-    elif j == "self.turn_right()":
-        if ori == ['1', '0']:
-            ori2 = ['0', '1']
-        elif ori == ['0', '1']:
-            ori2 = ['-1', '0']
-        elif ori == ['-1', '0']:
-            ori2 = ['0', '-1']
-        elif ori == ['0', '-1']:
-            ori2 = ['1', '0']
-        for fact in env.facts():
-            if str(fact).startswith("(ori "):
-                fact.retract()
-                orientation = "(ori " + str(ori2[0]) + " " + str(ori2[1]) + ")"
-                env.assert_string(orientation)
-
-    elif j == "self.turn_left()":
-        if ori == ['1', '0']:
-            ori2 = ['0', '-1']
-        elif ori == ['0', '-1']:
-            ori2 = ['-1', '0']
-        elif ori == ['-1', '0']:
-            ori2 = ['0', '1']
-        elif ori == ['0', '1']:
-            ori2 = ['1', '0']
-        for fact in env.facts():
-            if str(fact).startswith("(ori "):
-                fact.retract()
-                orientation = "(ori " + str(ori2[0]) + " " + str(ori2[1]) + ")"
-                env.assert_string(orientation)
-
-
 # Comprobar si el agente ha pasado por una misma posición en tres ocasiones
-def check_memory(memory, pos, env):
+def check_memory(memory, env):
+
+    position = ['0', '0']
+    for fact in env.facts():
+        if str(fact).startswith("(pos "):
+            position = str(fact)[:-1].split()[1:]
 
     found = False
     for i in memory:
-        if i[0] == pos:
+        if i[0] == position:
             i[1] += 1
             found = True
+            if i[1] >= 5:
+                env.assert_string("(caution_loop)")
 
     if not found:
-        memory.append([pos, 1])
-
-    for j in memory:
-        if j[1] >= 3:
-            env.assert_string("(caution_loop)")
-            j[1] = 0
+        memory.append([position, 1])
 
 
 def algorithm(objects, env, available=None):
@@ -160,10 +118,9 @@ def algorithm(objects, env, available=None):
 class Agent_CLIPS:
 
     # Se inicializa el entorno y se añaden las reglas CLIPS
-    def __init__(self, rules):
+    def __init__(self, path):
         self.env = clips.Environment()
-        for i in rules:
-            self.env.build(i)
+        self.env.load(path)
         self.env.reset()
         self.memory = []
 
@@ -175,13 +132,10 @@ class Agent_CLIPS:
         # Obtener las percepciones del agente
         walls, objects = what_I_see(agent)
 
-        # Obtener la posición del agente
-        pos, ori = where_I_am(self.env)
-
         # Crea el hecho de muros en el entorno CLIPS
         set_walls(self.env, walls)
 
         # Actualiza la memoria
-        check_memory(self.memory, pos, self.env)
+        check_memory(self.memory, self.env)
 
-        return walls, objects, pos, ori
+        return walls, objects
