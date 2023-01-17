55ff578910ef40150d56f4c7754a7d562eefb131
cgarcia-UCO <42171969+cgarcia-UCO@users.noreply.github.com>
2023-01-17 12:14:43 +0100

    Update rules_to_clips_v2.py

--- rules_to_clips_v2.py
+++ rules_to_clips_v2.py
@@ -11,6 +11,7 @@ def what_I_see(self):
 
 # Reiniciar el entorno CLIPS del agente
 def reset_environment(env):
+    env.strategy = clips.common.Strategy.RANDOM
     no_change = False
     while not no_change:
         no_change = True
@@ -37,3 +38,34 @@ def set_walls(env, walls):
             fact += " " + i
     fact += ")"
     env.assert_string(fact)
+    
+    
+def move(self):
+  # Elimina los hechos que no necesita del entorno CLIPS
+  reset_environment(env)
+
+  # Recoge las paredes y objetos que detecta el agente
+  walls, objects = what_I_see(self)
+
+  # Crear el hecho de las paredes en el entorno CLIPS
+  set_walls(env,walls)
+
+  # Crear las posibles funciones accesibles al agente
+  for i in objects:
+        for key in i:
+            if key.endswith('_function'):
+                object_function = "ob_" + str(id(i)) + "_funct"
+                available_objects = {object_function: i[key]}
+                new_object = '(available_object ' + str(
+                    i['type']) + ' "' + "available_objects" + "['" + object_function + "']" + '(self)")'
+                env.assert_string(new_object)
+
+  # Ejecutar pasos de agente
+  env.run(1)
+
+  # Ejecutar las opciones en pending_function_calls
+  for i, fact in enumerate(env.facts()):
+    if str(fact).startswith("(pending_function_calls"):
+      functions = str(fact)[:-1].split()[1:]
+      for j in functions:
+        eval(j.replace('"',''))
